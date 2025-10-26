8795d5dbee7870bac6e0207c50721094d8ef601b
PiterWeb <71133634+PiterWeb@users.noreply.github.com>
2025-10-26 18:25:46 +0100

    ffmpeg linux implementation (still not working)

--- src/ffmpeg_linux.v
+++ src/ffmpeg_linux.v
@@ -6,11 +6,25 @@ enum Options {
 }
 
 fn get_command(address string, option Options) string {
-
 	if option == Options.default || option == Options.x11grab {
-		
 	}
 
-	return "ffmpeg -f gdigrab -framerate 30 -offset_x 0 -offset_y 0 -video_size 1920x1080 -i desktop -c:v libx264 -preset ultrafast -tune zerolatency -f rtp rtp://${address} -sdp_file ./stream.sdp"
+	mut ffmpeg_command := 'sudo ffmpeg -hide_banner -nostats -loglevel quiet'
 
+	ffmpeg_command += ' '
+
+	desktop_src := if option == Options.default || option == Options.x11grab {
+		'-f x11grab -show_region 1 -framerate 25 -i :0.0'
+	} else {
+		''
+	}
+
+	ffmpeg_command += desktop_src
+	ffmpeg_command += ' '
+
+	rtp_stream := '-c:v libx264 -preset ultrafast -tune zerolatency -f rtp rtp://${address} -sdp_file ./stream.sdp'
+
+	ffmpeg_command += rtp_stream
+
+	return ffmpeg_command
 }
